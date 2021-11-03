61e4d5417731cb5446dbf4dbc8ea3ffc0522fa6b
Fábio Salomão Silva <fabio@arquivarnet.com.br>
2021-11-03 10:18:45 -0500

    Adicionar arquivos de projeto.

---
+++ SharpExtensions.Teste/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SharpExtensions.Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number = 2.11M;
+            Console.WriteLine(number.ToInt());
+
+            int[] arr = { 0, 1, 2, 3, 0, 1 };
+            Console.WriteLine(arr.FromArrayToString());
+            Console.WriteLine(arr.FromArrayToString("-"));
+
+            decimal[] arrDecimal = { 2.11M, 3.89M, 4.659M, 5.157M, 6.73429M, 7.2349M };
+            Console.WriteLine(arrDecimal.FromArrayToString());
+
+            int[] arrOrder = { 2, 3, 0, 1, 5, 4 };
+            Console.WriteLine(arrOrder.ToOrderBy().FromArrayToString());
+
+        }
+    }
+}
---
+++ SharpExtensions/Extensions.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace SharpExtensions
+{
+    public static class Extensions
+    {
+        public static int ToInt(this string value)
+        {
+            return Convert.ToInt32(value);
+        }
+        public static int ToInt(this decimal value)
+        {
+            return Convert.ToInt32(decimal.Round(value));
+        }
+        public static int ToInt(this double value)
+        {
+            return Convert.ToInt32(value);
+        }
+        public static short ToShort(this string value)
+        {
+            return Convert.ToInt16(value);
+        }
+        public static short ToShort(this decimal value)
+        {
+            return Convert.ToInt16(decimal.Round(value));
+        }
+        public static short ToShort(this double value)
+        {
+            return Convert.ToInt16(value);
+        }
+        public static long ToLong(this string value)
+        {
+            return Convert.ToInt64(value);
+        }
+        public static long ToLong(this decimal value)
+        {
+            return Convert.ToInt64(decimal.Round(value));
+        }
+        public static long ToLong(this double value)
+        {
+            return Convert.ToInt64(value);
+        }
+        public static double ToDouble(this string value)
+        {
+            return Convert.ToDouble(value);
+        }
+        public static double ToDouble(this int value)
+        {
+            return Convert.ToDouble(value);
+        }
+        public static double ToDouble(this decimal value)
+        {
+            return Convert.ToDouble(value);
+        }
+        public static double ToDouble(this float value)
+        {
+            return Convert.ToDouble(value);
+        }
+        public static DateTime ToDateTime(this string value)
+        {
+            return Convert.ToDateTime(value);
+        }
+        public static string FromArrayToString(this int[] value)
+        {
+            return string.Join(",", value);
+        }
+        public static string FromArrayToString(this string[] value)
+        {
+            return string.Join(",", value);
+        }
+        public static string FromArrayToString(this decimal[] value)
+        {
+            return string.Join(",", value);
+        }
+        public static string FromArrayToString(this double[] value)
+        {
+            return string.Join(",", value);
+        }
+        public static string FromArrayToString(this int[] value, string characterSeparator)
+        {
+            return string.Join(characterSeparator, value);
+        }
+        public static string FromArrayToString(this string[] value, string characterSeparator)
+        {
+            return string.Join(characterSeparator, value);
+        }
+        public static string FromArrayToString(this decimal[] value, string characterSeparator)
+        {
+            return string.Join(characterSeparator, value);
+        }
+        public static string FromArrayToString(this double[] value, string characterSeparator)
+        {
+            return string.Join(characterSeparator, value);
+        }
+        public static int[] ToOrderBy(this int[] value)
+        {
+            Array.Sort(value);
+            return value;
+        }
+
+    }
+}
